12f090434f78768cbce08419cad9bd97d2a5cae7
waynejiangnzit <wayne.jiang.nzit@gmail.com>
2019-06-06 10:06:37 +1200

    MRTest edition 1

---
+++ MRTest/MRTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MRTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MRTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47424d8c-6581-4d99-beec-38b00be0d4d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MRTest/MRTest/Scripts/ClassTee.js
@@ -0,0 +1,84 @@
+﻿
+//Show and hide size when click the size buttons
+function show(size) {
+    var showedValue = document.getElementById("chosenSize").innerHTML;
+    switch (size) {
+        case "s":
+            //If already chosen, clear the size and border
+            if (showedValue == "S") {
+                document.getElementById("chosenSize").innerHTML = "";
+                document.getElementById("buttonS").style.border = "1px solid #CCCCCC";
+            }
+            else {
+                document.getElementById("chosenSize").innerHTML = "S";
+                document.getElementById("buttonS").style.border = "2px solid #222222";
+                //clear other button border
+                document.getElementById("buttonM").style.border = "1px solid #CCCCCC";
+                document.getElementById("buttonL").style.border = "1px solid #CCCCCC";
+            }
+            break;
+        case "m":
+            //If already chosen, clear the size and border
+            if (showedValue == "M") {
+                document.getElementById("chosenSize").innerHTML = "";
+                document.getElementById("buttonM").style.border = "1px solid #CCCCCC";
+            }
+            else {
+                document.getElementById("chosenSize").innerHTML = "M";
+                document.getElementById("buttonM").style.border = "2px solid #222222";
+                //clear other button border
+                document.getElementById("buttonS").style.border = "1px solid #CCCCCC";
+                document.getElementById("buttonL").style.border = "1px solid #CCCCCC";
+            }
+            break;
+        case "l":
+            //If already chosen, clear the size and border
+            if (showedValue == "L") {
+                document.getElementById("chosenSize").innerHTML = "";
+                document.getElementById("buttonL").style.border = "1px solid #CCCCCC";
+            }
+            else {
+                document.getElementById("chosenSize").innerHTML = "L";
+                document.getElementById("buttonL").style.border = "2px solid #222222";
+                //clear other button border
+                document.getElementById("buttonM").style.border = "1px solid #CCCCCC";
+                document.getElementById("buttonS").style.border = "1px solid #CCCCCC";
+            }
+            break;
+        default:
+            document.getElementById("chosenSize").innerHTML = "";
+            document.getElementById("buttonS").style.border = "1px solid #CCCCCC";
+            document.getElementById("buttonM").style.border = "1px solid #CCCCCC";
+            document.getElementById("buttonL").style.border = "1px solid #CCCCCC";
+    }
+};
+
+
+function showCart() {
+    document.getElementById("popup").style.display = "block";
+};
+
+function hideCart() {
+    document.getElementById("popup").style.display = "none";
+};
+
+function addToCart() {
+    var showedValue = document.getElementById("chosenSize").innerHTML;
+    if (showedValue == "")
+        document.getElementById("errorMesg").innerHTML = "Please choose a size.";
+    else {
+        if (document.getElementById("errorMesg").innerHTML != "")
+            document.getElementById("errorMesg").innerHTML = "";
+        else {
+
+            /*
+            Functions that add order to the cart...
+            */
+
+        }
+
+    }
+
+};
+
+
